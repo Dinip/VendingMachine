cf70d3c154791a35a6bb84867d0678cb9e3bf902
Dinip <dinis_pimpao@hotmail.com>
2019-02-18 19:34:46 +0000

    keypad done

--- Vending/Keypad.Designer.vb
+++ Vending/Keypad.Designer.vb
@@ -23,6 +23,19 @@ Partial Class Keypad
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.keypad_screen_lbl = New System.Windows.Forms.Label()
+        Me.btn1 = New System.Windows.Forms.Button()
+        Me.btn2 = New System.Windows.Forms.Button()
+        Me.btn3 = New System.Windows.Forms.Button()
+        Me.btn4 = New System.Windows.Forms.Button()
+        Me.btn5 = New System.Windows.Forms.Button()
+        Me.btn6 = New System.Windows.Forms.Button()
+        Me.btn7 = New System.Windows.Forms.Button()
+        Me.btn8 = New System.Windows.Forms.Button()
+        Me.btn9 = New System.Windows.Forms.Button()
+        Me.btnX = New System.Windows.Forms.Button()
+        Me.btn0 = New System.Windows.Forms.Button()
+        Me.btnC = New System.Windows.Forms.Button()
+        Me.btnOK = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'keypad_screen_lbl
@@ -30,7 +43,8 @@ Partial Class Keypad
         Me.keypad_screen_lbl.BackColor = System.Drawing.Color.Transparent
         Me.keypad_screen_lbl.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
         Me.keypad_screen_lbl.Cursor = System.Windows.Forms.Cursors.No
-        Me.keypad_screen_lbl.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.keypad_screen_lbl.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.keypad_screen_lbl.ForeColor = System.Drawing.Color.Red
         Me.keypad_screen_lbl.ImageAlign = System.Drawing.ContentAlignment.MiddleRight
         Me.keypad_screen_lbl.Location = New System.Drawing.Point(80, 92)
         Me.keypad_screen_lbl.Margin = New System.Windows.Forms.Padding(5, 0, 5, 0)
@@ -38,8 +52,176 @@ Partial Class Keypad
         Me.keypad_screen_lbl.RightToLeft = System.Windows.Forms.RightToLeft.Yes
         Me.keypad_screen_lbl.Size = New System.Drawing.Size(186, 42)
         Me.keypad_screen_lbl.TabIndex = 0
-        Me.keypad_screen_lbl.Text = "Label1"
         Me.keypad_screen_lbl.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'btn1
+        '
+        Me.btn1.BackColor = System.Drawing.Color.Transparent
+        Me.btn1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.btn1.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btn1.FlatAppearance.BorderSize = 0
+        Me.btn1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btn1.Location = New System.Drawing.Point(76, 175)
+        Me.btn1.Name = "btn1"
+        Me.btn1.Size = New System.Drawing.Size(49, 43)
+        Me.btn1.TabIndex = 1
+        Me.btn1.UseVisualStyleBackColor = False
+        '
+        'btn2
+        '
+        Me.btn2.BackColor = System.Drawing.Color.Transparent
+        Me.btn2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.btn2.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btn2.FlatAppearance.BorderSize = 0
+        Me.btn2.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btn2.Location = New System.Drawing.Point(147, 175)
+        Me.btn2.Name = "btn2"
+        Me.btn2.Size = New System.Drawing.Size(49, 43)
+        Me.btn2.TabIndex = 2
+        Me.btn2.UseVisualStyleBackColor = False
+        '
+        'btn3
+        '
+        Me.btn3.BackColor = System.Drawing.Color.Transparent
+        Me.btn3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.btn3.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btn3.FlatAppearance.BorderSize = 0
+        Me.btn3.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btn3.Location = New System.Drawing.Point(217, 175)
+        Me.btn3.Name = "btn3"
+        Me.btn3.Size = New System.Drawing.Size(49, 43)
+        Me.btn3.TabIndex = 3
+        Me.btn3.UseVisualStyleBackColor = False
+        '
+        'btn4
+        '
+        Me.btn4.BackColor = System.Drawing.Color.Transparent
+        Me.btn4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.btn4.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btn4.FlatAppearance.BorderSize = 0
+        Me.btn4.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btn4.Location = New System.Drawing.Point(76, 234)
+        Me.btn4.Name = "btn4"
+        Me.btn4.Size = New System.Drawing.Size(49, 43)
+        Me.btn4.TabIndex = 4
+        Me.btn4.UseVisualStyleBackColor = False
+        '
+        'btn5
+        '
+        Me.btn5.BackColor = System.Drawing.Color.Transparent
+        Me.btn5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.btn5.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btn5.FlatAppearance.BorderSize = 0
+        Me.btn5.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btn5.Location = New System.Drawing.Point(147, 234)
+        Me.btn5.Name = "btn5"
+        Me.btn5.Size = New System.Drawing.Size(49, 43)
+        Me.btn5.TabIndex = 5
+        Me.btn5.UseVisualStyleBackColor = False
+        '
+        'btn6
+        '
+        Me.btn6.BackColor = System.Drawing.Color.Transparent
+        Me.btn6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.btn6.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btn6.FlatAppearance.BorderSize = 0
+        Me.btn6.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btn6.Location = New System.Drawing.Point(217, 234)
+        Me.btn6.Name = "btn6"
+        Me.btn6.Size = New System.Drawing.Size(49, 43)
+        Me.btn6.TabIndex = 6
+        Me.btn6.UseVisualStyleBackColor = False
+        '
+        'btn7
+        '
+        Me.btn7.BackColor = System.Drawing.Color.Transparent
+        Me.btn7.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.btn7.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btn7.FlatAppearance.BorderSize = 0
+        Me.btn7.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btn7.Location = New System.Drawing.Point(76, 293)
+        Me.btn7.Name = "btn7"
+        Me.btn7.Size = New System.Drawing.Size(49, 43)
+        Me.btn7.TabIndex = 7
+        Me.btn7.UseVisualStyleBackColor = False
+        '
+        'btn8
+        '
+        Me.btn8.BackColor = System.Drawing.Color.Transparent
+        Me.btn8.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.btn8.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btn8.FlatAppearance.BorderSize = 0
+        Me.btn8.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btn8.Location = New System.Drawing.Point(147, 293)
+        Me.btn8.Name = "btn8"
+        Me.btn8.Size = New System.Drawing.Size(49, 43)
+        Me.btn8.TabIndex = 8
+        Me.btn8.UseVisualStyleBackColor = False
+        '
+        'btn9
+        '
+        Me.btn9.BackColor = System.Drawing.Color.Transparent
+        Me.btn9.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.btn9.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btn9.FlatAppearance.BorderSize = 0
+        Me.btn9.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btn9.Location = New System.Drawing.Point(217, 293)
+        Me.btn9.Name = "btn9"
+        Me.btn9.Size = New System.Drawing.Size(49, 43)
+        Me.btn9.TabIndex = 9
+        Me.btn9.UseVisualStyleBackColor = False
+        '
+        'btnX
+        '
+        Me.btnX.BackColor = System.Drawing.Color.Transparent
+        Me.btnX.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.btnX.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btnX.FlatAppearance.BorderSize = 0
+        Me.btnX.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnX.Location = New System.Drawing.Point(76, 351)
+        Me.btnX.Name = "btnX"
+        Me.btnX.Size = New System.Drawing.Size(49, 43)
+        Me.btnX.TabIndex = 10
+        Me.btnX.UseVisualStyleBackColor = False
+        '
+        'btn0
+        '
+        Me.btn0.BackColor = System.Drawing.Color.Transparent
+        Me.btn0.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.btn0.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btn0.FlatAppearance.BorderSize = 0
+        Me.btn0.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btn0.Location = New System.Drawing.Point(147, 351)
+        Me.btn0.Name = "btn0"
+        Me.btn0.Size = New System.Drawing.Size(49, 43)
+        Me.btn0.TabIndex = 11
+        Me.btn0.UseVisualStyleBackColor = False
+        '
+        'btnC
+        '
+        Me.btnC.BackColor = System.Drawing.Color.Transparent
+        Me.btnC.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.btnC.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btnC.FlatAppearance.BorderSize = 0
+        Me.btnC.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnC.Location = New System.Drawing.Point(217, 351)
+        Me.btnC.Name = "btnC"
+        Me.btnC.Size = New System.Drawing.Size(49, 43)
+        Me.btnC.TabIndex = 12
+        Me.btnC.UseVisualStyleBackColor = False
+        '
+        'btnOK
+        '
+        Me.btnOK.BackColor = System.Drawing.Color.Transparent
+        Me.btnOK.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.btnOK.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btnOK.FlatAppearance.BorderSize = 0
+        Me.btnOK.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnOK.Location = New System.Drawing.Point(84, 446)
+        Me.btnOK.Name = "btnOK"
+        Me.btnOK.Size = New System.Drawing.Size(173, 51)
+        Me.btnOK.TabIndex = 13
+        Me.btnOK.UseVisualStyleBackColor = False
         '
         'Keypad
         '
@@ -48,6 +230,19 @@ Partial Class Keypad
         Me.BackgroundImage = Global.Vending.My.Resources.Resources.only_pad
         Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
         Me.ClientSize = New System.Drawing.Size(342, 633)
+        Me.Controls.Add(Me.btnOK)
+        Me.Controls.Add(Me.btnC)
+        Me.Controls.Add(Me.btn0)
+        Me.Controls.Add(Me.btnX)
+        Me.Controls.Add(Me.btn9)
+        Me.Controls.Add(Me.btn8)
+        Me.Controls.Add(Me.btn7)
+        Me.Controls.Add(Me.btn6)
+        Me.Controls.Add(Me.btn5)
+        Me.Controls.Add(Me.btn4)
+        Me.Controls.Add(Me.btn3)
+        Me.Controls.Add(Me.btn2)
+        Me.Controls.Add(Me.btn1)
         Me.Controls.Add(Me.keypad_screen_lbl)
         Me.Name = "Keypad"
         Me.Text = "Keypad"
@@ -56,4 +251,17 @@ Partial Class Keypad
     End Sub
 
     Friend WithEvents keypad_screen_lbl As Label
+    Friend WithEvents btn1 As Button
+    Friend WithEvents btn2 As Button
+    Friend WithEvents btn3 As Button
+    Friend WithEvents btn4 As Button
+    Friend WithEvents btn5 As Button
+    Friend WithEvents btn6 As Button
+    Friend WithEvents btn7 As Button
+    Friend WithEvents btn8 As Button
+    Friend WithEvents btn9 As Button
+    Friend WithEvents btnX As Button
+    Friend WithEvents btn0 As Button
+    Friend WithEvents btnC As Button
+    Friend WithEvents btnOK As Button
 End Class
--- Vending/Keypad.vb
+++ Vending/Keypad.vb
@@ -1,3 +1,157 @@
-﻿Public Class Keypad
+﻿Imports System.ComponentModel
 
+Public Class Keypad
+    Public Property numero As Integer
+    Private Sub Keypad_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' WIP keypad_screen_lbl.Text = numero
+
+        ' Button 1
+        btn1.FlatStyle = FlatStyle.Flat
+        btn1.BackColor = Color.Transparent
+        btn1.FlatAppearance.BorderSize = 0
+        btn1.FlatAppearance.MouseOverBackColor = Color.Transparent
+        btn1.FlatAppearance.MouseDownBackColor = Color.Transparent
+
+        ' Button 2
+        btn2.FlatStyle = FlatStyle.Flat
+        btn2.BackColor = Color.Transparent
+        btn2.FlatAppearance.BorderSize = 0
+        btn2.FlatAppearance.MouseOverBackColor = Color.Transparent
+        btn2.FlatAppearance.MouseDownBackColor = Color.Transparent
+
+        ' Button 3
+        btn3.FlatStyle = FlatStyle.Flat
+        btn3.BackColor = Color.Transparent
+        btn3.FlatAppearance.BorderSize = 0
+        btn3.FlatAppearance.MouseOverBackColor = Color.Transparent
+        btn3.FlatAppearance.MouseDownBackColor = Color.Transparent
+
+        ' Button 4
+        btn4.FlatStyle = FlatStyle.Flat
+        btn4.BackColor = Color.Transparent
+        btn4.FlatAppearance.BorderSize = 0
+        btn4.FlatAppearance.MouseOverBackColor = Color.Transparent
+        btn4.FlatAppearance.MouseDownBackColor = Color.Transparent
+
+        ' Button 5
+        btn5.FlatStyle = FlatStyle.Flat
+        btn5.BackColor = Color.Transparent
+        btn5.FlatAppearance.BorderSize = 0
+        btn5.FlatAppearance.MouseOverBackColor = Color.Transparent
+        btn5.FlatAppearance.MouseDownBackColor = Color.Transparent
+
+        ' Button 6
+        btn6.FlatStyle = FlatStyle.Flat
+        btn6.BackColor = Color.Transparent
+        btn6.FlatAppearance.BorderSize = 0
+        btn6.FlatAppearance.MouseOverBackColor = Color.Transparent
+        btn6.FlatAppearance.MouseDownBackColor = Color.Transparent
+
+        ' Button 7
+        btn7.FlatStyle = FlatStyle.Flat
+        btn7.BackColor = Color.Transparent
+        btn7.FlatAppearance.BorderSize = 0
+        btn7.FlatAppearance.MouseOverBackColor = Color.Transparent
+        btn7.FlatAppearance.MouseDownBackColor = Color.Transparent
+
+        ' Button 8
+        btn8.FlatStyle = FlatStyle.Flat
+        btn8.BackColor = Color.Transparent
+        btn8.FlatAppearance.BorderSize = 0
+        btn8.FlatAppearance.MouseOverBackColor = Color.Transparent
+        btn8.FlatAppearance.MouseDownBackColor = Color.Transparent
+
+        ' Button 9
+        btn9.FlatStyle = FlatStyle.Flat
+        btn9.BackColor = Color.Transparent
+        btn9.FlatAppearance.BorderSize = 0
+        btn9.FlatAppearance.MouseOverBackColor = Color.Transparent
+        btn9.FlatAppearance.MouseDownBackColor = Color.Transparent
+
+        ' Button 0
+        btn0.FlatStyle = FlatStyle.Flat
+        btn0.BackColor = Color.Transparent
+        btn0.FlatAppearance.BorderSize = 0
+        btn0.FlatAppearance.MouseOverBackColor = Color.Transparent
+        btn0.FlatAppearance.MouseDownBackColor = Color.Transparent
+
+        ' Button X
+        btnX.FlatStyle = FlatStyle.Flat
+        btnX.BackColor = Color.Transparent
+        btnX.FlatAppearance.BorderSize = 0
+        btnX.FlatAppearance.MouseOverBackColor = Color.Transparent
+        btnX.FlatAppearance.MouseDownBackColor = Color.Transparent
+
+        ' Button C
+        btnC.FlatStyle = FlatStyle.Flat
+        btnC.BackColor = Color.Transparent
+        btnC.FlatAppearance.BorderSize = 0
+        btnC.FlatAppearance.MouseOverBackColor = Color.Transparent
+        btnC.FlatAppearance.MouseDownBackColor = Color.Transparent
+
+
+        ' Button OK
+        btnOK.FlatStyle = FlatStyle.Flat
+        btnOK.BackColor = Color.Transparent
+        btnOK.FlatAppearance.BorderSize = 0
+        btnOK.FlatAppearance.MouseOverBackColor = Color.Transparent
+        btnOK.FlatAppearance.MouseDownBackColor = Color.Transparent
+    End Sub
+
+    Private Sub btn1_Click(sender As Object, e As EventArgs) Handles btn1.Click
+        keypad_screen_lbl.Text = keypad_screen_lbl.Text + "1"
+    End Sub
+
+    Private Sub btn2_Click(sender As Object, e As EventArgs) Handles btn2.Click
+        keypad_screen_lbl.Text = keypad_screen_lbl.Text + "2"
+    End Sub
+
+    Private Sub btn3_Click(sender As Object, e As EventArgs) Handles btn3.Click
+        keypad_screen_lbl.Text = keypad_screen_lbl.Text + "3"
+    End Sub
+
+    Private Sub btn4_Click(sender As Object, e As EventArgs) Handles btn4.Click
+        keypad_screen_lbl.Text = keypad_screen_lbl.Text + "4"
+    End Sub
+
+    Private Sub btn5_Click(sender As Object, e As EventArgs) Handles btn5.Click
+        keypad_screen_lbl.Text = keypad_screen_lbl.Text + "5"
+    End Sub
+
+    Private Sub btn6_Click(sender As Object, e As EventArgs) Handles btn6.Click
+        keypad_screen_lbl.Text = keypad_screen_lbl.Text + "6"
+    End Sub
+
+    Private Sub btn7_Click(sender As Object, e As EventArgs) Handles btn7.Click
+        keypad_screen_lbl.Text = keypad_screen_lbl.Text + "7"
+    End Sub
+
+    Private Sub btn8_Click(sender As Object, e As EventArgs) Handles btn8.Click
+        keypad_screen_lbl.Text = keypad_screen_lbl.Text + "8"
+    End Sub
+
+    Private Sub btn9_Click(sender As Object, e As EventArgs) Handles btn9.Click
+        keypad_screen_lbl.Text = keypad_screen_lbl.Text + "9"
+    End Sub
+
+    Private Sub btnX_Click(sender As Object, e As EventArgs) Handles btnX.Click
+        Me.Close()
+    End Sub
+
+    Private Sub btn0_Click(sender As Object, e As EventArgs) Handles btn0.Click
+        keypad_screen_lbl.Text = keypad_screen_lbl.Text + "0"
+    End Sub
+
+    Private Sub btnC_Click(sender As Object, e As EventArgs) Handles btnC.Click
+        keypad_screen_lbl.Text = ""
+    End Sub
+
+    Private Sub btnOK_Click(sender As Object, e As EventArgs) Handles btnOK.Click
+        Me.Close()
+        numero = Val(keypad_screen_lbl.Text)
+    End Sub
+
+    Private Sub Keypad_Closing(sender As Object, e As CancelEventArgs) Handles Me.Closing
+        MainInterface.Controls("keypad_screen_lbl").Text = keypad_screen_lbl.Text
+    End Sub
 End Class
