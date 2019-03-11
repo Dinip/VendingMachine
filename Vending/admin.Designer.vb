b10a6acd70e4a1ec6cc59c5033c9a261125b13d7
Dinip <videosdobest@gmail.com>
2019-03-11 17:27:08 +0000

    added admin form

---
+++ Vending/admin.Designer.vb
@@ -0,0 +1,80 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class admin
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.stock_lbl = New System.Windows.Forms.Label()
+        Me.restock_btn = New System.Windows.Forms.Button()
+        Me.price_lbl = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'stock_lbl
+        '
+        Me.stock_lbl.BackColor = System.Drawing.Color.Transparent
+        Me.stock_lbl.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.stock_lbl.ForeColor = System.Drawing.SystemColors.Control
+        Me.stock_lbl.Location = New System.Drawing.Point(220, 9)
+        Me.stock_lbl.Name = "stock_lbl"
+        Me.stock_lbl.Size = New System.Drawing.Size(84, 235)
+        Me.stock_lbl.TabIndex = 0
+        '
+        'restock_btn
+        '
+        Me.restock_btn.BackColor = System.Drawing.Color.Gray
+        Me.restock_btn.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.restock_btn.ForeColor = System.Drawing.SystemColors.Control
+        Me.restock_btn.Location = New System.Drawing.Point(103, 9)
+        Me.restock_btn.Name = "restock_btn"
+        Me.restock_btn.Size = New System.Drawing.Size(89, 69)
+        Me.restock_btn.TabIndex = 1
+        Me.restock_btn.Text = "Restock Machine"
+        Me.restock_btn.UseVisualStyleBackColor = False
+        '
+        'price_lbl
+        '
+        Me.price_lbl.BackColor = System.Drawing.Color.Transparent
+        Me.price_lbl.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.price_lbl.ForeColor = System.Drawing.SystemColors.Control
+        Me.price_lbl.Location = New System.Drawing.Point(12, 9)
+        Me.price_lbl.Name = "price_lbl"
+        Me.price_lbl.Size = New System.Drawing.Size(81, 235)
+        Me.price_lbl.TabIndex = 2
+        '
+        'admin
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.WindowFrame
+        Me.ClientSize = New System.Drawing.Size(316, 260)
+        Me.Controls.Add(Me.price_lbl)
+        Me.Controls.Add(Me.restock_btn)
+        Me.Controls.Add(Me.stock_lbl)
+        Me.Name = "admin"
+        Me.Text = "Admin Interface"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents stock_lbl As Label
+    Friend WithEvents restock_btn As Button
+    Friend WithEvents price_lbl As Label
+End Class
---
+++ Vending/admin.vb
@@ -0,0 +1,15 @@
+﻿Public Class admin
+    Private Sub admin_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        stock_lbl.Text = "Stocks:" & vbNewLine & "1 => " & stock(1) & vbNewLine & "2 => " & stock(2) & vbNewLine & "3 => " & stock(3) & vbNewLine & "4 => " & stock(4) & vbNewLine & "5 => " & stock(5) & vbNewLine & "6 => " & stock(6) & vbNewLine & "7 => " & stock(7) & vbNewLine & "8 => " & stock(8) & vbNewLine & "9 => " & stock(9) & vbNewLine & "10 => " & stock(10) & vbNewLine & "11 => " & stock(11) & vbNewLine & "12 => " & stock(12) & vbNewLine & "13 => " & stock(13)
+        price_lbl.Text = "Prices:" & vbNewLine & "1 => " & price(1) & vbNewLine & "2 => " & price(2) & vbNewLine & "3 => " & price(3) & vbNewLine & "4 => " & price(4) & vbNewLine & "5 => " & price(5) & vbNewLine & "6 => " & price(6) & vbNewLine & "7 => " & price(7) & vbNewLine & "8 => " & price(8) & vbNewLine & "9 => " & price(9) & vbNewLine & "10 => " & price(10) & vbNewLine & "11 => " & price(11) & vbNewLine & "12 => " & price(12) & vbNewLine & "13 => " & price(13)
+    End Sub
+
+    Private Sub restock_btn_Click(sender As Object, e As EventArgs) Handles restock_btn.Click
+
+        ' Stock manager
+        For num As Integer = 1 To 13
+            stock(num) = Int((8 * Rnd()) + 1)
+        Next
+        stock_lbl.Text = "Stocks:" & vbNewLine & "1 => " & stock(1) & vbNewLine & "2 => " & stock(2) & vbNewLine & "3 => " & stock(3) & vbNewLine & "4 => " & stock(4) & vbNewLine & "5 => " & stock(5) & vbNewLine & "6 => " & stock(6) & vbNewLine & "7 => " & stock(7) & vbNewLine & "8 => " & stock(8) & vbNewLine & "9 => " & stock(9) & vbNewLine & "10 => " & stock(10) & vbNewLine & "11 => " & stock(11) & vbNewLine & "12 => " & stock(12) & vbNewLine & "13 => " & stock(13)
+    End Sub
+End Class
